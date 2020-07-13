3bd43f8dd3730ac3d99198a9de7c08ae16c60459
renadvent <37464856+renadvent@users.noreply.github.com>
2020-07-12 19:06:30 -0700

    Articles Post Correctly

--- src/main/js/CommentComponents/Article.js
+++ src/main/js/CommentComponents/Article.js
@@ -90,7 +90,7 @@ function CreateArticleArea(props) {
                                        })
                                    })
                  })
-            
+
             setPost(false)
         }
     }, [sectionData, post, articleData])
