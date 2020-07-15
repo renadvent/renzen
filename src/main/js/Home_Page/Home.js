4c4b965a4d40b7691bf7c0e4f7ecd390af328e86
renadvent <37464856+renadvent@users.noreply.github.com>
2020-07-15 16:47:54 -0700

    about to move index.js

--- src/main/js/Home_Page/Home.js
+++ src/main/js/Home_Page/Home.js
@@ -5,6 +5,9 @@ import * as actionTypes from "../actions"
 import {log_in} from "../actions";
 
 import {connect} from 'react-redux'
+import ListSection from "./ListSection";
+import LoginRegisterSection from "./LoginRegistrationSection";
+import CreateCommunitySection from "./CreateCommunitySection";
 
 const mapDispatchToProps = dispatch =>{
     return {
@@ -14,25 +17,16 @@ const mapDispatchToProps = dispatch =>{
     }
 }
 
+const mapStateToProps = state => {
+    return{
+        userName: state.userName,
+        userURL: state.userURL
+    }
+}
+
 function Home(props) {
 
-    const [userName,setUserName] = useState()
-    const [password,setPassword] = useState()
-
     const [createCom,setCreateCom] = useState(false)
-
-    function handleRegister(props){
-        props.preventDefault();
-
-        Axios.post("/api/users",{
-            userName: userName,
-            password: password,
-            communities: ["default"],
-            contentList :[]
-        })
-        setPassword("")
-        setUserName("")
-    }
 
     function handleNewCommunity(props){
         Axios.post("/api/communities",{
@@ -52,232 +46,15 @@ function Home(props) {
             }}>Fake Login by Redux</button>
 
             <button onClick={handleCreateComClick}
-                //onClick={handleNewCommunity}
+                onClick={handleNewCommunity}
                     type="submit" className="btn btn-dark">Create New Community +</button>
             <div className={createCom ? 'd-block' : 'd-none'}>
                 <CreateCommunitySection/>
             </div>
-            <LoginRegisterSection/>
+            <LoginRegisterSection />
             <ListSection/>
         </div>
     )
-
-    function CreateCommunitySection(){
-
-        const [comData,setcomData] = useState({
-            name:"",
-            description:""
-            //articles:[],
-            //comDiscussionSections:"",
-            //users:"default",
-        })
-
-        function handleChange(event) {
-            const {value, name} = event.target
-            setcomData(prevState => {
-                return {
-                    ...prevState,
-                    [name]: value
-                }
-            })
-        }
-
-        function handleSubmit(event){
-
-            Axios.post("/api/communities",{
-                name:comData.name,
-                description:comData.description
-
-                //create other fields in server
-
-            })
-        }
-
-        return(
-            <div>
-
-                <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text" id="basic-addon3">Community Name</span>
-                    </div>
-                    <input
-                        name={"name"}
-                        value={comData.name}
-                        onChange={handleChange}
-                        type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3"/>
-                </div>
-
-
-                <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text" id="basic-addon3">Description</span>
-                    </div>
-                    <input
-                        name={"description"}
-                             value={comData.description}
-                             onChange={handleChange}
-
-
-                        type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3"/>
-                </div>
-
-                <button onClick={handleSubmit}
-                    type="submit" className="btn btn-dark">Add Community</button>
-
-                <hr/>
-
-            </div>
-        )
-    }
-
-    function ListSection(){
-
-        //load communities
-        const [allCommunities,setAllCommunities] = useState([])
-        useEffect(()=>{
-            Axios.get("/api/communities").then(comObjects=>{
-                // console.log(comObjects)
-                setAllCommunities( ()=> {
-                    return comObjects.data._embedded.communities.map(comObject=>{
-                        // console.log(comObject)
-                        return(<div><a href={comObject._links.self.href} >+{comObject.name}</a></div>)
-                        // return(<a href={comObject.data._links.self.href}>{comObject.data.name}</a>)
-                    })
-                    }
-                )
-            })
-        },[])
-
-        let counter =1
-
-        function getNewId() {
-            counter = counter + 1;
-            return counter - 1
-        }
-
-        function handleSelectArticle(event,url){
-
-            //create a new tab
-            event.preventDefault()
-            let id="C"+getNewId()
-            props.setTabs(prevState=>{
-                return prevState.concat(
-                    <li className="nav-item">
-                        <a className="nav-link" id={"T"+getNewId()} data-toggle="tab" href={"#"+id} role="tab"
-                           aria-controls="profile" aria-selected="false">url</a>
-                    </li>
-                )
-            })
-
-            props.setTabContent(prevState=>{
-                return prevState.concat(
-
-                    <div className="tab-pane fade" id={id} role="tabpanel"
-                         aria-labelledby="home-tab">
-                    <CreateArticleArea page={url} title={"Add Article"}/>
-                    </div>)
-            })
-                    }
-
-        //load articles
-        const [allArticles,setAllArticles] = useState([])
-        useEffect(()=>{
-            Axios.get("/api/articles").then(comObjects=>{
-                // console.log(comObjects)
-                setAllArticles( ()=> {
-                        return comObjects.data._embedded.articles.map(comObject=>{
-                            // console.log(comObject)
-                            return(<div>
-                                <a
-                                    onClick={(event) => handleSelectArticle(event,comObject._links.self.href)}
-                                >
-
-                                    {/*href={comObject._links.self.href} >*/}
-                                +{comObject.articleName}</a>
-                            </div>)
-                            // return(<a href={comObject.data._links.self.href}>{comObject.data.name}</a>)
-                        })
-                    }
-                )
-            })
-        },[])
-
-        //load users
-        const [allUsers,setAllUsers] = useState([])
-        useEffect(()=>{
-            Axios.get("/api/users").then(comObjects=>{
-                // console.log(comObjects)
-                setAllUsers( ()=> {
-                        return comObjects.data._embedded.users.map(comObject=>{
-                            //console.log(comObject)
-                            return(<div><a href={comObject._links.self.href} >{comObject.userName}</a></div>)
-                            // return(<a href={comObject.data._links.self.href}>{comObject.data.name}</a>)
-                        })
-                    }
-                )
-            })
-        },[])
-
-        return(
-            <div className="container-fluid">
-                <div className={"row"}>
-                    <div className={"col"}>
-                        <h4>All Users</h4>
-                        {allUsers}
-                    </div>
-                    <div className={"col"}>
-                        <h4>All Articles</h4>
-                        {allArticles}
-                    </div>
-                    <div className={"col"}>
-                        <h4>All Communities</h4>
-                        {allCommunities}
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
-    function LoginRegisterSection(){
-        return(
-            <div>
-        <form action="/login" method="POST">
-            <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input type="email" className="form-control" name="username"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" name="password"/>
-            </div>
-            <button type="submit" className="btn btn-dark">Login</button>
-        </form>
-
-        <hr/>
-
-        <form onSubmit={event => handleRegister(event)}>
-            <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input value={userName} onChange={(event => setUserName(event.target.value))}
-                       type="username" className="form-control" name="username"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input value={password} onChange={(event => setPassword(event.target.value))}
-                       type="password" className="form-control" name="password"/>
-            </div>
-            <button  type="submit" className="btn btn-dark">Register</button>
-        </form>
-            </div>
-        )
-    }
-}
-
-const mapStateToProps = state => {
-    return{
-        userName: state.userName,
-        userURL: state.userURL
-    }
 }
 
 export default connect(mapStateToProps,mapDispatchToProps)(Home)
---
+++ src/main/js/Home_Page/ListSection.js
@@ -0,0 +1,103 @@
+import React, {useEffect,useState} from "react"
+import Axios from "axios";
+import CreateArticleArea from "../Community_Page/Article";
+
+function ListSection(props){
+
+
+        //load communities
+        const [allCommunities,setAllCommunities] = useState([])
+        useEffect(()=>{
+            Axios.get("/api/communities").then(comObjects=>{
+                setAllCommunities( ()=> {
+                        return comObjects.data._embedded.communities.map(comObject=>{
+                            return(<div><a href={comObject._links.self.href} >+{comObject.name}</a></div>)
+                        })
+                    }
+                )
+            })
+        },[])
+
+        let counter =1
+
+        function getNewId() {
+            counter = counter + 1;
+            return counter - 1
+        }
+
+        function handleSelectArticle(event,url){
+
+            //create a new tab
+            event.preventDefault()
+            let id="C"+getNewId()
+            props.setTabs(prevState=>{
+                return prevState.concat(
+                    <li className="nav-item">
+                        <a className="nav-link" id={"T"+getNewId()} data-toggle="tab" href={"#"+id} role="tab"
+                           aria-controls="profile" aria-selected="false">url</a>
+                    </li>
+                )
+            })
+
+            props.setTabContent(prevState=>{
+                return prevState.concat(
+                    <div className="tab-pane fade" id={id} role="tabpanel"
+                         aria-labelledby="home-tab">
+                        <CreateArticleArea page={url} title={"Add Article"}/>
+                    </div>)
+            })
+        }
+
+        //load articles
+        const [allArticles,setAllArticles] = useState([])
+        useEffect(()=>{
+            Axios.get("/api/articles").then(comObjects=>{
+                setAllArticles( ()=> {
+                        return comObjects.data._embedded.articles.map(comObject=>{
+                            return(<div>
+                                <a
+                                    onClick={(event) => handleSelectArticle(event,comObject._links.self.href)}
+                                >
+                                    +{comObject.articleName}</a>
+                            </div>)
+                        })
+                    }
+                )
+            })
+        },[])
+
+        //load users
+        const [allUsers,setAllUsers] = useState([])
+        useEffect(()=>{
+            Axios.get("/api/users").then(comObjects=>{
+                setAllUsers( ()=> {
+                        return comObjects.data._embedded.users.map(comObject=>{
+                            return(<div><a href={comObject._links.self.href} >{comObject.userName}</a></div>)
+                        })
+                    }
+                )
+            })
+        },[])
+
+        return(
+            <div className="container-fluid">
+                <div className={"row"}>
+                    <div className={"col"}>
+                        <h4>All Users</h4>
+                        {allUsers}
+                    </div>
+                    <div className={"col"}>
+                        <h4>All Articles</h4>
+                        {allArticles}
+                    </div>
+                    <div className={"col"}>
+                        <h4>All Communities</h4>
+                        {allCommunities}
+                    </div>
+                </div>
+            </div>
+        )
+
+}
+
+export default ListSection
