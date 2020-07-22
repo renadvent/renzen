b7d9feb46473ead5bb4851ba5be6409119b108c5
renadvent <37464856+renadvent@users.noreply.github.com>
2020-07-21 21:50:18 -0700

    all ids now exposed for entities

--- src/main/java/com/ren/renzen/config.java
+++ src/main/java/com/ren/renzen/config.java
@@ -1,6 +1,6 @@
 package com.ren.renzen;
 
-import com.ren.renzen.Entities.Community;
+import com.ren.renzen.Entities.*;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -10,6 +10,7 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapt
 public class config implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.exposeIdsFor(Community.class);
+        config.exposeIdsFor(Community.class, Article.class, Content.class,
+                Page.class, Section.class, StudyGuide.class,User.class);
     }
 }
