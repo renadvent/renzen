55cd30604898f56a205b0f57449ee7d280b65ce0
renadvent <37464856+renadvent@users.noreply.github.com>
2020-09-07 00:03:16 -0700

    deleted unused files

--- src/main/old_components/JSONLoader.js
+++
@@ -1,9 +0,0 @@
-export function JSONLoader(props) {
-
-    return fetch(props, {method: 'GET'})
-        .then(response => response.json())
-}
-
-//most liikely will return an array of objects when used with MongoDB
-
-export default JSONLoader
--- src/main/old_components/PopOverButton.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-{/*<div id="hold">*/}
-{/*    <button data-placement="right" type="button" className="btn btn-lg btn-danger" data-toggle="popover" title="Popover title"*/}
-{/*            data-content="And here's some amazing content. It's very engaging. Right?" onClick={aClick}*/}
-{/*    data-container="#mid">Click to toggle popover*/}
-{/*    </button>*/}
-{/*</div>*/}
