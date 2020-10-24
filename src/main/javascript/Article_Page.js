50af0b8c5e6b574bcf216d69a005f26765007c36
renadvent <37464856+renadvent@users.noreply.github.com>
2020-10-24 11:32:01 -0700

    separated Article Page logic

--- src/main/javascript/Article_Page.js
+++ src/main/javascript/Article_Page.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import * as store from "./actions/Store_Actions";
 
-/*
+/**
 renders the article content as tab content
  */
 
@@ -21,7 +21,57 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 function Article_Page(props) {
-  function loginLogic() {
+  return (
+    <div
+      style={{ textAlign: "center" }}
+      className="tab-pane fade"
+      id={props.href}
+      role="tabpanel"
+      aria-labelledby="article-tab"
+    >
+      <br />
+
+      {bookmarkLogic()}
+
+      <h1>{props.payload.name}</h1>
+      <p>{props.payload.description}</p>
+      <p>by: {props.payload.userName}</p>
+
+      <hr />
+
+      {props.payload.articleSectionCOList.map((section) => {
+        return (
+          <div className="d-flex justify-content-center">
+            <div className="card" style={{ width: "60%" }}>
+              <div className="card-body">
+                <h5 className="card-title">{section.header}</h5>
+                <p className="card-text">{section.body}</p>
+
+                {imageRenderLogic(section)}
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+
+  /**
+   * Logic
+   * @returns {JSX.Element}
+   */
+
+  function imageRenderLogic(section) {
+    return section.imageID !== null ? (
+      <img
+        height={500}
+        src={section.imageID}
+        alt={"IMAGE HERE! " + section.imageID}
+      />
+    ) : null;
+  }
+
+  function bookmarkLogic() {
     return props.user.logged_in ? (
       props.user.bookmarks.find((x) => {
         return x._id === props.payload._id;
@@ -49,49 +99,6 @@ function Article_Page(props) {
       </div>
     );
   }
-
-  return (
-    <div
-      style={{ textAlign: "center" }}
-      className="tab-pane fade"
-      id={props.href}
-      role="tabpanel"
-      aria-labelledby="article-tab"
-    >
-      <br />
-
-      {loginLogic()}
-
-      <h1>{props.payload.name}</h1>
-      <p>{props.payload.description}</p>
-      <p>by: {props.payload.userName}</p>
-
-      <hr />
-
-      {props.payload.articleSectionCOList.map((section) => {
-        console.log(section);
-
-        return (
-          <div className="d-flex justify-content-center">
-            <div className="card" style={{ width: "60%" }}>
-              <div className="card-body">
-                <h5 className="card-title">{section.header}</h5>
-                <p className="card-text">{section.body}</p>
-
-                {section.imageID !== null ? (
-                  <img
-                    height={500}
-                    src={section.imageID}
-                    alt={"IMAGE HERE! " + section.imageID}
-                  />
-                ) : null}
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Article_Page);
